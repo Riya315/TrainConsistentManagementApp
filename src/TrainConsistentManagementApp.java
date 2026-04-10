9f9e69304a290859c558ec13c8addf87aca947a0
Riya315 <31bhargavariya@gmail.com>
2026-04-10 15:15:05 +0530

    updated TrainConsistentManagementApp

--- src/TrainConsistentManagementApp.java
+++ src/TrainConsistentManagementApp.java
@@ -1,67 +1,19 @@
-class PassengerBogie {
-    private String type;
-    private int capacity;
+// ---------------- Main Application ----------------
+import java.util.Arrays;
 
-    public PassengerBogie(String type, int capacity) {
-        this.type = type;
-        this.capacity = capacity;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    @Override
-    public String toString() {
-        return type + " (Capacity: " + capacity + ")";
-    }
-}
 
 public class TrainConsistentManagementApp {
-
     public static void main(String[] args) {
-
-        // Example passenger bogie capacities
-        PassengerBogie[] bogies = {
-                new PassengerBogie("Sleeper", 72),
-                new PassengerBogie("AC Chair", 56),
-                new PassengerBogie("First Class", 24),
-                new PassengerBogie("Sleeper", 70),
-                new PassengerBogie("AC Chair", 60)
-        };
+        // Example bogie type names
+        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
 
         System.out.println(" Before Sorting:");
-        displayBogies(bogies);
+        System.out.println(Arrays.toString(bogieNames));
 
-        bubbleSort(bogies);
+        // Sort using Arrays.sort()
+        Arrays.sort(bogieNames);
 
-        System.out.println("\n After Sorting (Ascending by Capacity):");
-        displayBogies(bogies);
-    }
-
-    // Bubble Sort Algorithm
-    public static void bubbleSort(PassengerBogie[] bogies) {
-        int n = bogies.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (bogies[j].getCapacity() > bogies[j + 1].getCapacity()) {
-                    // Swap
-                    PassengerBogie temp = bogies[j];
-                    bogies[j] = bogies[j + 1];
-                    bogies[j + 1] = temp;
-                }
-            }
-        }
-    }
-
-    // Display Bogies
-    public static void displayBogies(PassengerBogie[] bogies) {
-        for (PassengerBogie bogie : bogies) {
-            System.out.println(bogie);
-        }
+        System.out.println("\n After Sorting (Alphabetical Order):");
+        System.out.println(Arrays.toString(bogieNames));
     }
 }
